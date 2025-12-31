cf2319ce18d5e765e24dcf3e9cdd02ce0ba1e480
E HASINI <hasini1530@gmail.com>
2025-12-31 14:04:30 +0530

    Implement SHAP waterfall plot visualization
    Add a waterfall plot for SHAP values visualization.

--- Explainable_AI_Demo.py
+++ Explainable_AI_Demo.py
@@ -8,3 +8,12 @@ explainer=shap.TreeExplainer(model)
 shap_values=explainer.shap_values(x_train)
 #global explaination
 shap.summary_plot(shap_values,x_train)
+#waterfall plot
+shap.waterfall_plot(
+    shap.Explanation(
+        values=shap_values[0],
+        base_values=explainer.expected_value,
+        data=x_test.iloc[0],
+        feature_names=x_test.columns
+    )
+)
